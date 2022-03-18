78bd587455cfd8d295a7a755a3f263909e1d4aa4
Aleixo-Dev <nicolasaleixo2020@gmail.com>
2022-03-18 16:23:24 -0300

    new call from data source to repository

--- app/src/main/java/br/com/nicolas/brasilconsulta/data/repository/MainRepository.kt
+++ app/src/main/java/br/com/nicolas/brasilconsulta/data/repository/MainRepository.kt
@@ -2,6 +2,8 @@ package br.com.nicolas.brasilconsulta.data.repository
 
 import br.com.nicolas.brasilconsulta.common.Resource
 import br.com.nicolas.brasilconsulta.data.datasource.response.CepResponse
+import br.com.nicolas.brasilconsulta.data.datasource.response.FeriadosResponse
+import br.com.nicolas.brasilconsulta.data.datasource.response.FeriadosResponseItem
 import kotlinx.coroutines.flow.Flow
 
 interface MainRepository {
@@ -9,5 +11,7 @@ interface MainRepository {
     suspend fun fetchCep(cep: String): Flow<Resource<CepResponse>>
 
     suspend fun fetchDirect(direct: String): Flow<Resource<List<String>>>
+
+    suspend fun fetchHolidays(year: String): Flow<Resource<FeriadosResponse>>
 
 }
--- app/src/main/java/br/com/nicolas/brasilconsulta/data/repository/MainRepositoryImpl.kt
+++ app/src/main/java/br/com/nicolas/brasilconsulta/data/repository/MainRepositoryImpl.kt
@@ -1,8 +1,11 @@
 package br.com.nicolas.brasilconsulta.data.repository
 
+import br.com.nicolas.brasilconsulta.common.DateFormat
 import br.com.nicolas.brasilconsulta.common.Resource
 import br.com.nicolas.brasilconsulta.data.datasource.remote.RemoteDataSource
 import br.com.nicolas.brasilconsulta.data.datasource.response.CepResponse
+import br.com.nicolas.brasilconsulta.data.datasource.response.FeriadosResponse
+import br.com.nicolas.brasilconsulta.data.datasource.response.FeriadosResponseItem
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
@@ -12,27 +15,45 @@ class MainRepositoryImpl(
     private val remote: RemoteDataSource
 ) : MainRepository {
 
-    override suspend fun fetchCep(cep: String): Flow<Resource<CepResponse>> = flow {
-        try {
-            emit(Resource.Loading)
-            val response = remote.getCep(cep)
-            emit(Resource.Success(response))
-        } catch (exception: HttpException) {
-            emit(Resource.Error(exception.message() ?: "no internet connection."))
-        } catch (exception: IOException) {
-            emit(Resource.Error(exception.toString()))
+    override suspend fun fetchCep(cep: String): Flow<Resource<CepResponse>> =
+        flow {
+            try {
+                emit(Resource.Loading)
+                val response = remote.getCep(cep)
+                emit(Resource.Success(response))
+            } catch (exception: HttpException) {
+                emit(Resource.Error(exception.message() ?: "no internet connection."))
+            } catch (exception: IOException) {
+                emit(Resource.Error(exception.toString()))
+            }
         }
-    }
 
-    override suspend fun fetchDirect(direct: String): Flow<Resource<List<String>>> = flow {
-        try {
-            emit(Resource.Loading)
-            val response = remote.getDirect(direct)
-            emit(Resource.Success(response.cities))
-        } catch (exception: HttpException) {
-            emit(Resource.Error(exception.message() ?: "no internet connection."))
-        } catch (exception: IOException) {
-            emit(Resource.Error(exception.toString()))
+    override suspend fun fetchDirect(direct: String): Flow<Resource<List<String>>> =
+        flow {
+            try {
+                emit(Resource.Loading)
+                val response = remote.getDirect(direct)
+                emit(Resource.Success(response.cities))
+            } catch (exception: HttpException) {
+                emit(Resource.Error(exception.message() ?: "no internet connection."))
+            } catch (exception: IOException) {
+                emit(Resource.Error(exception.toString()))
+            }
         }
-    }
+
+    override suspend fun fetchHolidays(year: String): Flow<Resource<FeriadosResponse>> =
+        flow {
+            try {
+                emit(Resource.Loading)
+                val response = remote.getHolidays(year)
+                response.map {
+                    it.date = DateFormat.formatDateFromApi(it.date)
+                }
+                emit(Resource.Success(response))
+            } catch (exception: HttpException) {
+                emit(Resource.Error(exception.message() ?: "no internet connection."))
+            } catch (exception: IOException) {
+                emit(Resource.Error(exception.toString()))
+            }
+        }
 }
